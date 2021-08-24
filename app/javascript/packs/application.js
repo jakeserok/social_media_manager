e5401252ca019b156d4c73ecca3ff4c29801f0b5
jakeserok <jakeserok@gmail.com>
2021-08-23 23:40:35 -0400

    JS to update clock automatically added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,22 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+window.addEventListener('load', calculateTime)
+
+function calculateTime(){
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+
+  hour = hour % 12;
+  hour = hour ? hour : '12';
+  hour = hour < 10 ? '0' + hour : hour;
+  minute = minute < 10 ? '0' + minute : minute;
+
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("minute").innerHTML = minute;
+  
+  setTimeout(calculateTime, 200);
+}
